1d6be8207e31ce48285ef481f2899baaa5e9f621
Gopinath Pal <laptopasusgopinath@gmail.com>
2026-01-11 13:53:31 +0530

    add static root

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    'blog_main/static',
 ]
 
 MEDIA_URL = '/media/'
